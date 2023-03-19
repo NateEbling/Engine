f243b316b21be8823f679bc71e2cdfb019443fd0
Nate <ebling.nathan@gmail.com>
2023-03-19 15:35:14 -0400

    Added boundingbox for sprites

--- Assets/Sprites/Sprite.cs
+++ Assets/Sprites/Sprite.cs
@@ -7,17 +7,41 @@ namespace Engine
     public class Sprite
     {
         protected Texture2D texture;
-
         public bool isActive = true;
         public Vector2 position, startPosition = new Vector2(-1, -1), direction = new Vector2(1, 0); // Default dir right
         public float scale = 1f, rotation = 0f, layerDepth = 0.5f;
         public Color drawColor = Color.White;
+
+        protected int boundingBoxWidth, boundingBoxHeight;
+        protected Vector2 boundingBoxOffset, center;
+        private Texture2D boundingBoxTexture;
+        const bool drawBoundingBoxes = true;
+
 
         public Sprite() {}
 
         public Sprite(Texture2D texture)
         {
             this.texture = texture;
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                return new Rectangle((int)(position.X + boundingBoxOffset.X), (int)(position.Y + boundingBoxOffset.Y),
+                                     boundingBoxWidth, boundingBoxHeight);
+            }
+        }
+
+        public Rectangle Rectangle
+        {
+            get { return new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height); }
+        }
+
+        public virtual bool CheckCollisions(Rectangle input)
+        {
+            return BoundingBox.Intersects(input);
         }
 
         public virtual void Initialize()
@@ -28,7 +52,15 @@ namespace Engine
 
         public virtual void Load(ContentManager content)
         {
-            
+            boundingBoxTexture = content.Load<Texture2D>("boundingBox");
+
+            CalculateCenter();
+
+            if (texture != null)
+            {
+                boundingBoxWidth = texture.Width;
+                boundingBoxHeight = texture.Height;
+            }
         }
 
         public virtual void Update()
@@ -38,6 +70,11 @@ namespace Engine
 
         public virtual void Draw(SpriteBatch spriteBatch) 
         {
+            if (boundingBoxTexture != null && drawBoundingBoxes == true && isActive == true)
+            {
+                spriteBatch.Draw(boundingBoxTexture, new Vector2(BoundingBox.X, BoundingBox.Y), BoundingBox,
+                                 new Color(120, 120, 120, 120), 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.1f);
+            }
             if (isActive == true)
             {
                 spriteBatch.Draw(texture, position, null, drawColor, rotation, Vector2.Zero, scale, SpriteEffects.None, 
@@ -45,6 +82,14 @@ namespace Engine
             }
         }
 
-
+        private void CalculateCenter()
+        {
+            if (texture == null)
+                return;
+    
+            center.X = texture.Width / 2;
+            center.Y = texture.Height / 2;
+            
+        }
     }
 }
