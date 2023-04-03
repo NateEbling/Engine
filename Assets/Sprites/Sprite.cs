dc6b038e56f02367a7b28aad05e7776465566d71
Nate <ebling.nathan@gmail.com>
2023-04-03 11:57:43 +0200

    Added decor, map, and textureLoader classes

--- Assets/Sprites/Sprite.cs
+++ Assets/Sprites/Sprite.cs
@@ -7,7 +7,7 @@ namespace Engine
     public class Sprite
     {
         protected Texture2D texture;
-        public bool isActive = true;
+        public bool isActive = true, collidable = true;
         public Vector2 position, startPosition = new Vector2(-1, -1), direction = new Vector2(1, 0); // Default dir right
         public float scale = 1f, rotation = 0f, layerDepth = 0.5f;
         public Color drawColor = Color.White;
---
+++ Assets/World/Map.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Engine;
+public class Map
+{
+    // public List<Decor> decor = new List<Decor>();
+}
